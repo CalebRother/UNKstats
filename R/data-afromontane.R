349a6b3a6c312c681ec5f823b78d7eeac701db1b
CalebRother <rotherc@lopers.unk.edu>
2025-11-07 13:48:13 -0600

    Adding data

---
+++ R/data-afromontane.R
@@ -0,0 +1,21 @@
+#' Afromontane species presence–absence matrix
+#'
+#' Species presence–absence data from Cooper (2021),
+#' “Hierarchical analyses of community biogeography in the Afromontane highlands,”
+#' *Frontiers of Biogeography*.
+#'
+#' @format A data frame with 825 rows and 60 columns:
+#' \describe{
+#'   \item{From Dowsett (1986)}{Indicator (1/0) showing inclusion from Dowsett (1986)}
+#'   \item{From Bowie (2003)}{Indicator (1/0) showing inclusion from Bowie (2003)}
+#'   \item{Exclude}{Flag indicating whether a taxon should be excluded}
+#'   \item{Order}{Taxonomic order}
+#'   \item{Genus}{Taxonomic genus}
+#'   \item{Superspecies}{Superspecies grouping}
+#'   \item{Species}{Species epithet}
+#'   \item{Group}{Taxonomic or biogeographic grouping}
+#'   \item{Subspecies}{Subspecific classification, if any}
+#'   \item{Upper Guinea Highlands, Bioko, Mt. Cameroon, Cameroon Highlands, Bamenda & Adamawa, Monte Alen, ...}{Presence–absence (1 = present, 0 = absent) across 50+ Afromontane regions.}
+#' }
+#' @source Cooper, J.C. (2021). *Hierarchical analyses of community biogeography in the Afromontane highlands.* Frontiers of Biogeography. DOI: 10.21425/F5FBG51310
+"afromontane"
